675edbe79a6de54b84d49cfbf2d0cb871edf0487
Alex Boyne-Aitken <abaitken@live.co.uk>
2025-12-18 11:18:47 +0000

    feat: Basic add event implementation

--- src/js/addevent.js
+++ src/js/addevent.js
@@ -1,15 +1,47 @@
 import { Modal } from './modal.js';
 
-export class AddEventModel extends Modal {    
+class EventModel {
+    name;
+    datePattern;
+    color;
+    icon;
+
+    constructor() {
+        this.name = ko.observable('');
+        this.datePattern = ko.observable('');
+        this.color = ko.observable('');
+        this.icon = ko.observable('');
+    }
+
+    asSimpleObject() {
+        return {
+            text: this.name(),
+            date: this.datePattern(),
+            color: this.color(),
+            icon: this.icon()
+        };
+    }
+}
+
+export class AddEventModel extends Modal {
+    event;
+    calendar;
+
     constructor(elementId, parent) {
         super(elementId);
+        this.calendar = parent.calendar;
+        this.event = ko.observable(new EventModel());
     }
 
     addEvent() {
+        this.calendar.addEvent(this.event().asSimpleObject());
+        this.calendar.updateEvents();
+        this.event(new EventModel());
         this.close();
     }
 
     cancelAddEvent() {
+        this.event(new EventModel());
         this.close();
     }
 }
