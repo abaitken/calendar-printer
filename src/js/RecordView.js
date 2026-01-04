4d7dd1e77faa9088506a4fbbcfa3fb4ab4471c39
Alex Boyne-Aitken <abaitken@live.co.uk>
2026-01-04 12:30:21 +0000

    feat: Implemented basic sorting

--- src/js/RecordView.js
+++ src/js/RecordView.js
@@ -1,3 +1,62 @@
+class Heading {
+    direction;
+    sortFn;
+    name;
+    sortable;
+    resetting;
+
+    constructor(parent, options) {
+        this.sortFn = (options.sort) ? options.sort : (l, r) => {
+            const lv = l[options.key];
+            const rv = r[options.key];
+
+            if(typeof lv === 'number') {
+                return lv - rv;
+            }
+
+            if(typeof lv === 'string') {
+                return lv.localeCompare(rv);
+            }
+            return 0;
+        };
+        this.name = options.name;
+        const self = this;
+        const keys = Object.keys(options);
+        this.sortable = (keys.includes('sortable') && !options.sortable) ? false : true,
+        this.direction = ko.observable(0);
+        this.direction.subscribe(function(){
+            if(self.resetting) {
+                return;
+            }
+            parent.onSort(self);
+        });
+        this.resetting = false;
+    }
+
+    getNewDirection(current) {
+        switch(current) {
+            case 0:
+                return 1;
+            case 1:
+                return -1;
+            case -1:
+                return 0;
+        }
+
+        return 0;
+    }
+
+    changeDirection() {
+        this.direction(this.getNewDirection(this.direction()));
+    }
+
+    reset() {
+        this.resetting = true;
+        this.direction(0);
+        this.resetting = false;
+    }
+}
+
 export class RecordView {
     recordData;
     page;
@@ -10,11 +69,29 @@ export class RecordView {
     totalRecords;
     eventRouter;
     filterFn;
+    headings;
 
-    constructor(fetchRecordsFn, eventRouter, filterFn) {
+
+        // this.sort = {
+        //     'text': new SortModel(this, (l, r) => { }),
+        //     'important': new SortModel(this, (l, r) => { }),
+        //     'pattern': new SortModel(this, (l, r) => { }),
+        //     'color': new SortModel(this, (l, r) => { })
+        // };
+        // const keys = Object.keys(this.sort);
+        //     (l, r) => {
+        //         if(keys.every(o => self.sort[o].direction() === 0)) {
+        //             return 0;
+        //         }
+
+        //         return 0;
+        //     }
+
+    constructor(fetchRecordsFn, eventRouter, filterFn, headings) {
         this.fetchRecordsFn = fetchRecordsFn;
         this.eventRouter = eventRouter;
         this.filterFn = filterFn;
+        this.headings = headings.map(o => new Heading(this, o));
         this.recordData = ko.observableArray(fetchRecordsFn());
         this.page = ko.observable(0);
         this.recordsPerPage = ko.observable(10);
@@ -22,8 +99,18 @@ export class RecordView {
 
         const self = this;
 
+        this.sortedRecords = ko.computed(function() {
+            let recordData = self.recordData();
+            const sortOn = self.headings.filter(o => o.direction() != 0);
+            if(sortOn.length != 0 && sortOn[0].sortFn != null) {
+                recordData = [...recordData].sort((l, r) => sortOn[0].sortFn(l, r) * sortOn[0].direction());
+            }
+            return recordData;
+        });
+
         this.filteredRecords = ko.computed(function() {
-            const recordData = self.recordData().filter(o => self.filterFn(o));
+            const recordData = self.sortedRecords()
+                .filter(o => self.filterFn(o));
             return recordData;
         });
 
@@ -83,6 +170,14 @@ export class RecordView {
 
     lastPage() {
         this.page(this.calculateLastPage(this.totalRecords(), this.recordsPerPage()));
+    }
+
+    onSort(heading) {
+        const previousSort = this.headings.filter(o => o.direction() != 0 && o != heading);
+        if(previousSort.length !== 0) {
+            previousSort[0].reset();
+        }
+        this.refresh();
     }
 
     refresh() {
