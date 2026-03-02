6c56e43a934a109e11ed09215b78a84ee0c5939b
Alex Boyne-Aitken <abaitken@live.co.uk>
2026-03-02 09:55:25 +0000

    Removed python app

--- src/CalendarPrinter/DateRange.py
+++
@@ -1,6 +0,0 @@
-from .YearMonth import YearMonth
-
-class DateRange:
-    def __init__(self, jobj):
-        self.start = YearMonth.Parse(jobj['start'])
-        self.end = YearMonth.Parse(jobj['end'])
--- src/CalendarPrinter/TagMapping.py
+++
@@ -1,8 +0,0 @@
-from .Color import Color
-
-class TagMapping:
-    def __init__(self, jobj):
-        self.icon = jobj['icon']
-        self.tags = jobj['tags']
-        self.color = Color(jobj['color'])
-        
